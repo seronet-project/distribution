823aae06f366bb9c73367f8c4efb4e350c92a74d
Felix Penzlin <felix.penzlin@kuka.com>
2019-06-24 16:33:57 +0000

    ci startup script

--- ci/build.sh
+++ ci/build.sh
@@ -12,3 +12,10 @@ make
 
 wget -O certs.json https://sso.sero.network/auth/realms/RobotOne/protocol/openid-connect/certs
 cat <(echo "-----BEGIN CERTIFICATE-----") <(jq -r '.keys[0].x5c[0]' certs.json) <(echo "-----END CERTIFICATE-----") > cert.pem
+cp ./bin/registry ../distribution-output/
+cp ./bin/digest ../distribution-output/
+cp ./bin/registry-api-descriptor-template ../distribution-output/
+cp ./ci/manifest.yml ../distribution-output/
+cp ./ci/start.sh ../distribution-output/
+cp ./ci/config.yml.template ../distribution-output/
+cp cert.pem ../distribution-output/
