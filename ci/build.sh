bae30e1437862165e76c0355168891ab385259a1
Felix Penzlin <felix.penzlin@kuka.com>
2019-06-24 17:21:47 +0000

    ci build process

--- ci/build.sh
+++ ci/build.sh
@@ -3,8 +3,10 @@ cd ~
 wget https://dl.google.com/go/go1.12.5.linux-amd64.tar.gz
 tar -zxf go1.12.5.linux-amd64.tar.gz
 mv go /usr/local
+ls /usr/local/go
 export GOPATH=/app
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+mkdir -p /app/src/github.com/docker
 ln -s /distribution/ /app/src/github.com/docker/distribution
 cd /app/src/github.com/docker/distribution/
 make
