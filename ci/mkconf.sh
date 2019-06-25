d413e0d7432449172e0a343310d54443fd792a7d
Felix Penzlin <felix.penzlin@kuka.com>
2019-06-25 17:02:36 +0000

    default env value

--- ci/mkconf.sh
+++ ci/mkconf.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-sed -e "s/PORT/$PORT/" \
--e "s,WEBHOOK,$WEBHOOK," \
+sed -e "s/PORT/${PORT:-8080}/" \
+-e "s,WEBHOOK,${WEBHOOK:-https://chat.sero.network}," \
 -e "s/AWSACCESSKEY/$AWS_ACCESS_KEY_ID/" \
 -e "s/AWSSECRETKEY/$AWS_SECRET_ACCESS_KEY/" \
 -e "s,AWSENDPOINT,$AWS_ENDPOINT," \
