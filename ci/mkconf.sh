1bf89aebc090daaa0835533077f97c1fcccea79c
Felix Penzlin <felix.penzlin@kuka.com>
2019-07-02 18:48:02 +0200

    fixed ci auth issuer

--- ci/mkconf.sh
+++ ci/mkconf.sh
@@ -8,9 +8,9 @@ sed -e "s/PORT/${PORT:-8080}/" \
 -e "s/AWSSECRETKEY/$AWS_SECRET_ACCESS_KEY/" \
 -e "s,AWSENDPOINT,$AWS_ENDPOINT," \
 -e "s/AWSBUCKETNAME/$AWS_BUCKET/" \
--e "s/AUTHSERVICE/$AUTH_SERVICE/" \
+-e "s,AUTHSERVICE,$AUTH_SERVICE," \
 -e "s/AUTHREALM/$AUTH_REALM/" \
--e "s/AUTHISSUER/$AUTH_ISSUER/" \
+-e "s,AUTHISSUER,$AUTH_ISSUER," \
 config.yml.template > config.yml
 
 echo "PORT set to ${PORT:-8080}"
